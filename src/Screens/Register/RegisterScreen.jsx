bb9f4bb5a3d5bf8c44ba5dbba8e152dba7521c23
fwefew123 <artem.andreev200012@mail.ru>
2024-01-18 11:48:48 +0600

    version 1.0.0

--- src/Screens/Register/RegisterScreen.jsx
+++ src/Screens/Register/RegisterScreen.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import '../Register/style.scss'
+
 const RegisterScreen = () => {
-  return <div></div>;
+  return <>
+    <section className="reg-mob">
+      <nav className="reg">
+        <div className="reg__main">
+          <h1>Зарегистрироваться</h1>
+        </div>
+        <div className="reg__inputs">
+          <div className="reg__inputs__f">
+            <p>Ваше имя</p>
+            <input type="text" placeholder="Введите ваше имя" />
+          </div>
+          <div className="reg__inputs__f">
+            <p>Ваш пароль</p>
+            <input type="text" placeholder="Введите ваш пароль" />
+          </div>
+          <div className="reg__inputs__f">
+            <p>Повторно введите пароль</p>
+            <input type="text" placeholder="Введите пароль повторно" />
+          </div>
+        </div>
+      </nav>
+    </section></>
 };
 
 export default RegisterScreen;
