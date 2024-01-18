dc46e858fa26f64a1726618c93ec27d3d75bbee2
fwefew123 <artem.andreev200012@mail.ru>
2024-01-18 23:13:43 +0600

    version 1.0.0

--- src/Screens/Login/LoginScreen.jsx
+++ src/Screens/Login/LoginScreen.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./style.scss";
 
 const LoginScreen = () => {
-  return <div>Добро</div>;
+  return (
+    <div className="login">
+      <div className="reg__inputs">
+        <h1 className="login__main">Войти</h1>
+        <div className="reg__inputs__f">
+          <p className="reg__inputs__text">Ваше имя</p>
+          <input
+            className="reg__inputs__inp"
+            type="text"
+            placeholder="Введите ваше имя"
+          />
+        </div>
+        <div className="reg__inputs__f">
+          <p className="reg__inputs__text">Ваш пароль</p>
+          <input
+            className="reg__inputs__inp"
+            type="text"
+            placeholder="Введите ваш пароль"
+          />
+        </div>
+      </div>
+      <div className="login__items">
+        <button className="reg__items__btn">Войти</button>
+        <p className="reg__items__al">
+          Еще не зарегистрировались?
+          <Link className="reg__items__al-l" to={"/register"}>
+            Регистрация
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default LoginScreen;
--- src/Screens/Register/RegisterScreen.jsx
+++ src/Screens/Register/RegisterScreen.jsx
@@ -1,30 +1,56 @@
 import React from "react";
 
-import '../Register/style.scss'
+import { Link } from "react-router-dom";
+
+import "../Register/style.scss";
 
 const RegisterScreen = () => {
-  return <>
-    <section className="reg-mob">
-      <nav className="reg">
-        <div className="reg__main">
-          <h1>Зарегистрироваться</h1>
-        </div>
-        <div className="reg__inputs">
-          <div className="reg__inputs__f">
-            <p>Ваше имя</p>
-            <input type="text" placeholder="Введите ваше имя" />
+  return (
+    <>
+      <section className="reg-mob">
+        <nav className="reg">
+          <div className="reg__main">
+            <h1>Зарегистрироваться</h1>
           </div>
-          <div className="reg__inputs__f">
-            <p>Ваш пароль</p>
-            <input type="text" placeholder="Введите ваш пароль" />
+          <div className="reg__inputs">
+            <div className="reg__inputs__f">
+              <p className="reg__inputs__text">Ваше имя</p>
+              <input
+                className="reg__inputs__inp"
+                type="text"
+                placeholder="Введите ваше имя"
+              />
+            </div>
+            <div className="reg__inputs__f">
+              <p className="reg__inputs__text">Ваш пароль</p>
+              <input
+                className="reg__inputs__inp"
+                type="text"
+                placeholder="Введите ваш пароль"
+              />
+            </div>
+            <div className="reg__inputs__f">
+              <p className="reg__inputs__text">Повторно введите пароль</p>
+              <input
+                className="reg__inputs__inp"
+                type="text"
+                placeholder="Введите пароль повторно"
+              />
+            </div>
           </div>
-          <div className="reg__inputs__f">
-            <p>Повторно введите пароль</p>
-            <input type="text" placeholder="Введите пароль повторно" />
+          <div className="reg__items">
+            <button className="reg__items__btn">Зарегистрироваться</button>
+            <p className="reg__items__al">
+              Уже есть Аккаунт?
+              <Link className="reg__items__al-l" to={"/login"}>
+                Войти
+              </Link>
+            </p>
           </div>
-        </div>
-      </nav>
-    </section></>
+        </nav>
+      </section>
+    </>
+  );
 };
 
 export default RegisterScreen;
